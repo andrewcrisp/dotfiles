a5e9dad816a16056ba7e2d325f91d460f8ec7a98
Andrew Crisp <andrew.crisp@gmail.com>
2024-10-31 21:31:19 -0600

    Add oh-my-posh

--- setup_scripts/001-install.ps1
+++ setup_scripts/001-install.ps1
@@ -4,6 +4,7 @@ winget install --id Microsoft.PowerShell --source winget
 Install-Module -Name CompletionPredictor
 cp ./Microsoft.PowerShell_profile.ps1 $PROFILE
 
+winget install JanDeDobbeleer.OhMyPosh -s winget
 winget install Neovim.Neovimwinget install Neovim.Neovim
 winget install BurntSushi.ripgrep.MSVC
 
