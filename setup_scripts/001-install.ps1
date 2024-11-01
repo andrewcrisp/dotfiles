ce4d76bab99734bf8fd9be1d8d132fe5bc552ecd
Andrew Crisp <andrew.crisp@gmail.com>
2024-10-31 21:44:00 -0600

    Include missing Terminal-Icons and typo on nvim

--- setup_scripts/001-install.ps1
+++ setup_scripts/001-install.ps1
@@ -2,9 +2,10 @@
 
 winget install --id Microsoft.PowerShell --source winget
 Install-Module -Name CompletionPredictor
+Install-Module -Name Terminal-Icons
 cp ./Microsoft.PowerShell_profile.ps1 $PROFILE
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
-winget install Neovim.Neovimwinget install Neovim.Neovim
+winget install Neovim.Neovim
 winget install BurntSushi.ripgrep.MSVC
 
